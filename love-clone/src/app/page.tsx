2e61c150b5b9e7bf8e62d7faade6bcc68248cf02
Monkey Tikez <davidgao1345@gmail.com>
2025-12-28 08:30:27 -1000

    connect DB

--- love-clone/src/app/page.tsx
+++ love-clone/src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  
   return (
     <div>
-      <Button variant="outline">Click me</Button>
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
