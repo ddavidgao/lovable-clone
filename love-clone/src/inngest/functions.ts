7b2a072bc917691e766d0b566b9c200450390615
Monkey Tikez <davidgao1345@gmail.com>
2025-12-29 22:19:09 -1000

    hooked up grok AI

--- love-clone/src/inngest/functions.ts
+++ love-clone/src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { grok, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+        name: "code-agent",
+        system: "You are an expert next.js developer.  You write readable, maintainable, and efficient code. You write simple Next.js and React snippets.",
+        model: grok({ model:"grok-4-1-fast-reasoning", apiKey: process.env.XAI_API_KEY}),
+      });
+
+      const { output } = await codeAgent.run(
+        `Write the following snippet: ${event.data.value}`,
+      );
+      console.log(output);
+
+    return { output };
   },
 );
